d98fe2b227fee366f7ccd1208610a104a490d050
Daniel <airinei.daniel27@gmail.com>
2023-03-26 16:05:54 +0300

    first commit

---
+++ src/test/java/com/danielairinei/project/ProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.danielairinei.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
